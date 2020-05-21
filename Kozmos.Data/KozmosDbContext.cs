1b30bdf99fd33b8edf8cc34e467ba28e09bc8206
Hloni Matsoso <hloni2010@gmail.com>
2020-05-21 19:37:57 +0200

    Initial commit

---
+++ Kozmos.Data/KozmosDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Kozmos.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kozmos.Data
+{
+    public class KozmosDbContext : IdentityDbContext<KozmosUser>
+    {
+        public KozmosDbContext(DbContextOptions<KozmosDbContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
---
+++ Kozmos/Models/KozmosUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kozmos.Models
+{
+    public class KozmosUser : IdentityUser
+    {
+        public string City { get; set; }
+
+    }
+}
